0b390c1f2ffb1122316dfd135ea20bf3af930ae1
DESKTOP-R2GRGRO\User <sandy.tangps@gmail.com>
2023-10-18 16:04:01 +0800

    overall run

--- calculator.py
+++ calculator.py
@@ -11,7 +11,7 @@ def divide(num1, num2):
     try:
         return num1 / num2
     except ZeroDivisionError:  # need to add this error
-        return "Cannot divide by zero"
+        return "Cannot divide by 0"
 
 def powerOf(num1, num2):
     return num1 ** num2
